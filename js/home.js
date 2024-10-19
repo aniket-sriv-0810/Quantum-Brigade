b0d73a9088aa887d428137d23bc3c27d178fa83c
LUAMICIFER <ankitraj88099a@gmail.com>
2024-10-19 09:01:05 +0530

    counter added

--- js/home.js
+++ js/home.js
@@ -7,3 +7,20 @@ if (Username) {
 } else {
     document.getElementById('welcomeMessage').textContent = 'Hello, Guest!';
 }
+const counts = document.querySelectorAll('.count')
+const speed = 97
+
+counts.forEach((counter) => {
+    function upDate(){
+        const target = Number(counter.getAttribute('data-target'))
+        const count = Number(counter.innerText)
+        const inc = target / speed        
+        if(count < target){
+            counter.innerText = Math.floor(inc + count) 
+            setTimeout(upDate, 15)
+        }else{
+            counter.innerText = target
+        }
+    }
+    upDate()
+})
